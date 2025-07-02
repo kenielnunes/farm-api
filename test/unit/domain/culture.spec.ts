fb34944c1e3a067cc4d856e341df51f18a45c0f2
kenielnunes <kenielalves3@gmail.com>
2025-07-02 14:47:25 -0300

    feat: culture domain test validate name

--- test/unit/domain/culture.spec.ts
+++ test/unit/domain/culture.spec.ts
@@ -3,17 +3,27 @@ import { Culture } from 'src/modules/cultures/domain/entities/culture';
 import { AppException } from 'src/shared/exceptions/app.exception';
 
 describe('Entidade Cultura', () => {
-  const anoAtual = new Date().getFullYear();
+  const currentYear = new Date().getFullYear();
 
   it('deve criar uma cultura válida', () => {
     const culture = new Culture(
       faker.string.uuid(),
       faker.commerce.product(),
       faker.number.int({ min: 1, max: 100 }),
-      anoAtual,
+      currentYear,
       faker.string.uuid(),
     );
     expect(culture).toBeInstanceOf(Culture);
+  });
+
+  it('deve lançar erro se o nome da safra for vazio', () => {
+    expect(() => new Culture(
+      faker.string.uuid(),
+      '',
+      faker.number.int({ min: 1, max: 100 }),
+      currentYear,
+      faker.string.uuid(),
+    )).toThrow();
   });
 
   it('deve lançar exceção se o ano da safra for menor que o ano atual', () => {
@@ -22,7 +32,7 @@ describe('Entidade Cultura', () => {
         faker.string.uuid(),
         faker.commerce.product(),
         faker.number.int({ min: 1, max: 100 }),
-        anoAtual - 1,
+        currentYear - 1,
         faker.string.uuid(),
       );
     }).toThrow(AppException);
@@ -34,7 +44,7 @@ describe('Entidade Cultura', () => {
         faker.string.uuid(),
         faker.commerce.product(),
         faker.number.int({ min: 1, max: 100 }),
-        anoAtual + 2,
+        currentYear + 2,
         faker.string.uuid(),
       );
     }).toThrow(AppException);
@@ -46,7 +56,7 @@ describe('Entidade Cultura', () => {
         faker.string.uuid(),
         faker.commerce.product(),
         0,
-        anoAtual,
+        currentYear,
         faker.string.uuid(),
       );
     }).toThrow(AppException);
@@ -55,7 +65,7 @@ describe('Entidade Cultura', () => {
         faker.string.uuid(),
         faker.commerce.product(),
         -5,
-        anoAtual,
+        currentYear,
         faker.string.uuid(),
       );
     }).toThrow(AppException);
