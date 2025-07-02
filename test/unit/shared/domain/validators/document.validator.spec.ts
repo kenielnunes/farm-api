3fc0c620762b13033e516a2f0936c99c301a9e12
kenielnunes <kenielalves3@gmail.com>
2025-07-02 13:25:54 -0300

    refactor:  tests structure

--- test/unit/shared/domain/validators/document.validator.spec.ts
+++ test/unit/shared/domain/validators/document.validator.spec.ts
@@ -1,34 +1,28 @@
 import { faker } from "@faker-js/faker/.";
-import { DocumentValidatorService } from "src/modules/producers/domain/services/document-validator.service";
+import { DocumentValidator } from "src/shared/domain/validators/document-validator.service";
 import { AppException } from "src/shared/exceptions/app.exception";
 
-describe('DocumentValidatorService', () => {
-  let service: DocumentValidatorService;
-
-  beforeEach(() => {
-    service = new DocumentValidatorService();
-  });
-
+describe('DocumentValidator', () => {
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(DocumentValidator).toBeDefined();
   });
 
   describe('validate', () => {
     it('should not throw for a valid CPF', () => {
       const validCPF = '52998224725';
-      expect(() => service.validate(validCPF)).not.toThrow();
+      expect(() => DocumentValidator.validate(validCPF)).not.toThrow();
     });
 
     it('should throw AppException for invalid CPF', () => {
       const invalidCPF = '111.111.111-11';
-      expect(() => service.validate(invalidCPF)).toThrow(
+      expect(() => DocumentValidator.validate(invalidCPF)).toThrow(
         new AppException('INVALID_CPF', 'CPF inválido', 400),
       );
     });
 
     it('should throw AppException for invalid document length', () => {
       const invalidDoc = faker.string.numeric(5);
-      expect(() => service.validate(invalidDoc)).toThrow(
+      expect(() => DocumentValidator.validate(invalidDoc)).toThrow(
         new AppException(
           'INVALID_DOCUMENT',
           'Documento inválido. Deve ser um CPF (11 dígitos) ou CNPJ (14 dígitos)',
